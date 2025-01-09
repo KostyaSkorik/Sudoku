997fc7a41f326cf69882af1308ae5044bb3b3dbb
Enda Teddy <endatedd@student.21-school.ru>
2025-01-09 10:36:03 +0300

    make start field

--- sudoko.c
+++ sudoko.c
@@ -1,25 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define SIZE 9
 // printf("%ld\n",rand()%9);
 int **make_field();
 void free_mem(int **field);
 void output(int **field);
+int generate_random_num(int start, int end, int exclude[]);
+int is_contains_in_array(int num, int array[]);
+void transposition(int **field);
+void random_swap_row_and_colom(int **field);
+void shake_field(int** field);
+void generate_basic_field(int **field);
 
 int check_square(int **field, int ind_i, int ind_j, long num);
 int check_row(int **field, int ind, long num);
 void regenerate_num(int **field, int *ind_i, int *ind_j, long *num);
 int check_colom(int **field, int ind_j, long num);
 int check_uniq(int **field);
+void make_zero_field(int **field);
 
-void generate(int **field);
 int main() {
+  srand(time(NULL));
   int **field = make_field(SIZE);
-  generate(field);
+  generate_basic_field(field);
+  shake_field(field);  
   output(field);
-  printf("UNIQ is %d\n", check_uniq(field));
   free_mem(field);
   return 0;
+}
+
+void output(int **field) {
+  for (int i = 0; i < SIZE; i++) {
+    int flag_line = 0;
+    for (int j = 0; j < SIZE; j++) {
+      if (i % 3 == 0 && i != 0 && flag_line != 1) {
+        flag_line = 1;
+        for (int k = 0; k < SIZE + 10; k++) {
+
+          printf("-");
+        }
+        printf("\n");
+      }
+
+      if (j % 3 == 0 && j != 0) {
+        printf("|");
+      }
+
+      printf("%d ", field[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void generate_basic_field(int **field) {
+  int num;
+  for (int i = 0; i < SIZE; i++) {
+    if (i % 3 != 0) {
+      num = field[i - 1][3];
+    } else if (i % 3 == 0 && i != 0) {
+      num = field[i - 3][1];
+    } else {
+      num = generate_random_num(1, 9, NULL);
+      // num = 8;
+    }
+    for (int j = 0; j < SIZE; j++) {
+      field[i][j] = num;
+      num++;
+      if (num == 10) {
+        num = 1;
+      }
+    }
+  }
+}
+
+int generate_random_num(int start, int end, int exclude[]) {
+  int num = (start + rand() % (end - start + 1));
+  if (exclude != NULL && (exclude[0] != 0) && (start != end)) {
+    while (is_contains_in_array(num, exclude)) {
+      num = (start + rand() % (end - start + 1));
+    }
+  }
+
+  return num;
+}
+
+void transposition(int **field) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = i; j < SIZE; j++) {
+      int temp = field[i][j];
+      field[i][j] = field[j][i];
+      field[j][i] = temp;
+    }
+  }
+}
+
+void random_swap_row_and_colom(int **field) {
+  int exclude[6] = {1, 2, 4, 5, 7, 8};
+  int first_i = generate_random_num(0, 6, exclude);
+  int plus_num = generate_random_num(1, 2, NULL);
+  int second_i = first_i + plus_num;
+  int temp;
+  for (int j = 0; j < SIZE; j++) {
+    temp = field[second_i][j];
+    field[second_i][j] = field[first_i][j];
+    field[first_i][j] = temp;
+  }
+  for (int j = 0; j < SIZE; j++) {
+    temp = field[j][second_i];
+    field[j][second_i] = field[j][first_i];
+    field[j][first_i] = temp;
+  }
+}
+
+void shake_field(int** field){
+  int number_of_rep = generate_random_num(1, 100000,NULL);
+  for ( int i = 0; i < number_of_rep; i++){
+    transposition(field);
+    random_swap_row_and_colom(field);
+  
+  }
+}
+
+
+int is_contains_in_array(int num, int array[]) {
+  int flag = 0;
+  for (int i = 0; i < 20; i++) {
+    if (array[i] == num) {
+      flag = 1;
+    }
+  }
+  return flag;
 }
 
 int **make_field() {
@@ -36,42 +147,6 @@ void free_mem(int **field) {
     free(field[i]);
   }
   free(field);
-}
-
-void output(int **field) {
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
-      if ((j + 1) % 3 == 0) {
-        printf("%d|", field[i][j]);
-      }
-
-      else {
-        printf("%d ", field[i][j]);
-      }
-    }
-    printf("\n");
-  }
-}
-
-void generate(int **field) {
-  long r_num;
-
-  int i = 0;
-  int j = 0;
-  while (i < SIZE) {
-    while (j < SIZE) {
-      r_num = (rand() % 10);
-      if (r_num == 0) {
-        r_num += 1;
-      }
-      regenerate_num(field, &i, &j, &r_num);
-      field[i][j] = r_num;
-      output(field);
-      j++;
-    }
-    j = 0;
-    i++;
-  }
 }
 
 int check_row(int **field, int ind, long num) {
@@ -137,7 +212,8 @@ void regenerate_num(int **field, int *ind_i, int *ind_j, long *num) {
     }
     *ind_i = 0;
     *ind_j = 0;
-    printf("NUM = %ld\n", *num);
+    // make_zero_field(field);
+    // printf("NUM = %ld\n", *num);
     // generate(field);
   }
 }
@@ -155,4 +231,12 @@ int check_uniq(int **field) {
     }
   }
   return flag;
+}
+
+void make_zero_field(int **field) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      field[i][j] = 0;
+    }
+  }
 }
